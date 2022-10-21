6ebc59d33c656c57c96c15f8b4b1d1391fa6e2e9
Michael Dippery <michael@monkey-robot.com>
2022-10-21 15:11:59 -0700

    Process errors when opening invalid files

--- src/zless/__main__.py
+++ src/zless/__main__.py
@@ -1,17 +1,32 @@
+import sys
+from typing import NoReturn
+
 import click
 from simple_term_menu import TerminalMenu
-from zless.archive import Archive
+
+from zless.archive import Archive, ReadError
+
+
+def die(msg: str, code: int) -> NoReturn:
+    click.echo(msg, err=True)
+    sys.exit(code)
 
 
 @click.command()
 @click.argument("file")
 def zless(file: str) -> None:
-    archive = Archive(file)
-    contents = archive.contents
-    menu = TerminalMenu([entry.name for entry in archive.contents])
-    idx = menu.show()
-    entry = contents[idx]
-    click.echo_via_pager(archive.read(entry))
+    try:
+        archive = Archive(file)
+    except FileNotFoundError as exc:
+        die(f"No such file: {file!r}", 1)
+    except ReadError as exc:
+        die(str(exc), 1)
+    else:
+        contents = archive.contents
+        menu = TerminalMenu([entry.name for entry in archive.contents])
+        idx = menu.show()
+        entry = contents[idx]
+        click.echo_via_pager(archive.read(entry))
 
 
 if __name__ == "__main__":
--- src/zless/archive.py
+++ src/zless/archive.py
@@ -2,8 +2,14 @@ import tarfile
 from typing import IO, List, cast
 
 
+class ReadError(Exception):
+    pass
+
+
 class Archive:
     def __init__(self, path: str) -> None:
+        if not tarfile.is_tarfile(path):
+            raise ReadError(f"Could not open archive: {path!r}")
         self.path = path
 
     @property
