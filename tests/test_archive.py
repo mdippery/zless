5a9500a60869f9456e9227f9c5f76a4079454801
Michael Dippery <michael@monkey-robot.com>
2022-10-23 22:58:18 -0700

    Rename "archive" fixture to "path"
    It makes it a bit more clear what the fixture yields, and it avoids a
    possible name clash with the zless.archive module.

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -6,42 +6,28 @@ from zless.archive import Archive, ReadError
 
 
 class ArchiveTestSuite:
-    def test_archive_instantiation_with_gzipped_tarball(self, tarball):
-        ar = Archive(tarball)
-        assert ar.path == tarball
-
-    def test_archive_instantiation_with_nonexistent_tarball(self, tarball):
-        tarball = tarball.name
-        with pytest.raises(FileNotFoundError):
-            _ = Archive(tarball)
-
-    def test_archive_instantiation_with_invalid_tarball(self):
-        path = Path(__file__)
-        with pytest.raises(ReadError):
-            _ = Archive(path)
-
-    def test_archive_content_listing(self, tarball, contents):
-        actual = [e.name for e in Archive(tarball).contents]
+    def test_archive_content_listing(self, path, contents):
+        actual = [e.name for e in Archive(path).contents]
         assert actual == contents
 
-    def test_archive_open(self, tarball):
-        with Archive(tarball).open() as ar:
+    def test_archive_open(self, path):
+        with Archive(path).open() as ar:
             assert ar is not None
 
     @pytest.mark.skip(reason="not yet implemented")
-    def test_read_single_archive_entry(self, tarball):
+    def test_read_single_archive_entry(self, path):
         # TODO: Create valid TarInfo object to pass to read()
         assert False
 
-    def test_read_single_archive_entry_using_string(self, tarball):
+    def test_read_single_archive_entry_using_string(self, path):
         base = "PKG-INFO"
-        with open(tarball.parent / base) as fh:
+        with open(path.parent / base) as fh:
             expected = fh.read()
-        actual = Archive(tarball).read(f"zless-22.1.dev0/{base}")
+        actual = Archive(path).read(f"zless-22.1.dev0/{base}")
         assert actual == expected
 
-    def test_read_nonxistent_archive_entry(self, tarball):
-        ar = Archive(tarball)
+    def test_read_nonxistent_archive_entry(self, path):
+        ar = Archive(path)
         with pytest.raises(KeyError):
             _ = ar.read("README.rst")
 
@@ -58,14 +44,28 @@ class TarballTestSuite(ArchiveTestSuite):
             "zless-22.1.dev0/PKG-INFO",
         ]
 
+    def test_archive_instantiation_with_gzipped_tarball(self, path):
+        ar = Archive(path)
+        assert ar.path == path
+
+    def test_archive_instantiation_with_nonexistent_tarball(self, path):
+        path = path.name
+        with pytest.raises(FileNotFoundError):
+            _ = Archive(path)
+
+    def test_archive_instantiation_with_invalid_tarball(self):
+        path = Path(__file__)
+        with pytest.raises(ReadError):
+            _ = Archive(path)
+
 
 class TestGzippedTarball(TarballTestSuite):
     @pytest.fixture(scope="class")
-    def tarball(self):
+    def path(self):
         yield Path(__file__).parent / "fixtures" / "zless-22.1.dev0.tar.gz"
 
 
 class TestTarball(TarballTestSuite):
     @pytest.fixture(scope="class")
-    def tarball(self):
+    def path(self):
         yield Path(__file__).parent / "fixtures" / "zless-22.1.dev0.tar"
