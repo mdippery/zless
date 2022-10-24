ee91376982bc3f6c5fe0f793e17118b2a0714204
Michael Dippery <michael@monkey-robot.com>
2022-10-24 11:52:29 -0700

    Apply Black's linting changes

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -10,7 +10,7 @@ from zless.paths import output_path, to_path
     [
         (__file__, Path(__file__)),
         (Path(__file__), Path(__file__)),
-    ]
+    ],
 )
 def test_conversion_of_path(input_path, output_path):
     actual = to_path(input_path)
@@ -24,7 +24,7 @@ def test_conversion_of_path(input_path, output_path):
         ("zless-22.1.dev0/PKG-INFO", "/tmp/blah", Path("/tmp/blah")),
         ("zless-22.1.dev0/PKG-INFO", "/tmp/", Path("/tmp/PKG-INFO")),
         ("zless-22.1.dev0/PKG-INFO", "/tmp", Path("/tmp/PKG-INFO")),
-    ]
+    ],
 )
 def test_calculation_of_output_path(source, dest, expected):
     actual = output_path(source, dest)
